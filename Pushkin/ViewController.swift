4e330ebc1e6adeac45acd8b9a159dad5c3a7122c
Alexey Salangin <a7ls@ya.ru>
2019-09-27 18:19:06 +0300

    Create Xcode project

---
+++ Pushkin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pushkin
+//
+//  Created by Alexey Salangin on 9/27/19.
+//  Copyright © 2019 Alexey Salangin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
