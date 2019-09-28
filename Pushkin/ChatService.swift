739e1f6d8bd7854a165dc8f3d8ff17acd764176d
Alexey Salangin <a7ls@ya.ru>
2019-09-29 00:11:52 +0300

    Update ChatService

--- Pushkin/ChatService.swift
+++ Pushkin/ChatService.swift
@@ -13,7 +13,73 @@ enum ChatError: Error {
     case unknown
 }
 
-typealias Answer = String
+protocol ChatMessage {}
+
+struct PlainTextMessage: ChatMessage {
+    let text: String
+}
+
+struct ImageMessage: ChatMessage {
+    let imageURL: String
+}
+
+struct SoundMessage: ChatMessage {
+    let audioURL: String
+}
+
+struct CoordsMessage: ChatMessage {
+    let latitude: Double
+    let longitude: Double
+}
+
+struct ChatResponse: Decodable {
+    let image_url: String?
+    let coords: String?
+    let response: String?
+}
+
+extension ChatResponse {
+    func mapToChatMessages() -> [ChatMessage] {
+        var result: [ChatMessage] = []
+
+        if let text = response {
+            result.append(PlainTextMessage(text: text))
+        }
+
+        if let imageURL = image_url {
+            result.append(ImageMessage(imageURL: imageURL))
+        }
+
+        if let coordsString = coords {
+            let coordsArray = coordsString.split(separator: ",").compactMap({ Double($0) })
+            if coordsArray.count == 2, let latitude = coordsArray.first, let longitude = coordsArray.last {
+                result.append(CoordsMessage(latitude: latitude, longitude: longitude))
+            }
+        }
+        return result
+    }
+}
+
+extension Data {
+    func parseToChatResponses() -> [ChatResponse] {
+        let decoder = JSONDecoder()
+        do {
+            let result = try decoder.decode([ChatResponse].self, from: self)
+            return result
+        } catch {
+            print(error)
+            return []
+        }
+    }
+}
+
+extension Array where Element == ChatResponse {
+    func mapToChatMessages() -> [ChatMessage] {
+        return self.flatMap { $0.mapToChatMessages() }
+    }
+}
+
+typealias Answer = [ChatMessage]
 typealias AnswerHandler = (Result<Answer, ChatError>) -> Void
 
 protocol ChatService: AnyObject {
@@ -28,7 +94,7 @@ final class MockChatService: ChatService {
             if self.isFailure {
                 handler(.failure(.unknown))
             } else {
-                handler(.success("Hello!"))
+                handler(.success([]))
             }
             return
         }
@@ -36,7 +102,7 @@ final class MockChatService: ChatService {
 }
 
 final class ChatServiceImpl: NSObject, URLSessionDelegate, ChatService {
-    static private let urlString = "http://95.213.38.6:1488"
+    static private let urlString = "https://c278d005.ngrok.io"
 
     lazy private var session: URLSession = {
         let sessionConfiguration = URLSessionConfiguration.default
@@ -61,9 +127,14 @@ final class ChatServiceImpl: NSObject, URLSessionDelegate, ChatService {
             if let data = data,
                 let httpResponse = response as? HTTPURLResponse,
                 httpResponse.statusCode == 200 {
-                let answer = String(decoding: data, as: UTF8.self)
-                handler(.success(answer))
+                let answer = data.parseToChatResponses().mapToChatMessages()
+                if answer.isEmpty {
+                    handler(.failure(.unknown))
+                } else {
+                    handler(.success(answer))
+                }
             } else {
+                print("Status code: \((response as? HTTPURLResponse)?.statusCode ?? 0)")
                 handler(.failure(.unknown))
             }
         }.resume()
